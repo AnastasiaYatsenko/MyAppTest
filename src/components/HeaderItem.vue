439c85b5db86979fed7c4af924479516fb15bda8
Anastasiya Pozemina <a.pozyomina@voodoo.rocks>
2025-11-11 21:25:03 +0300

    Restyle header

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -13,5 +13,13 @@
   align-items: center;
   justify-content: space-between;
   color: $black;
+  margin-bottom: 35px;
+}
+
+h1 {
+  font-weight: 700;
+  font-size: 2rem;
+  line-height: 140%;
+  letter-spacing: 0;
 }
 </style>
