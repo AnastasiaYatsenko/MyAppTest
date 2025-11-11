ad484bfc55337dd41f4759eeed2266ec2df2e807
Anastasiya Pozemina <a.pozyomina@voodoo.rocks>
2025-11-11 22:07:25 +0300

    Set documents store

---
+++ src/stores/documents.ts
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia'
+import type { Document } from '@/types/documents'
+import { fetchDocuments as fetchDocumentsApi } from '@/api/documents'
+
+export const useDocumentsStore = defineStore('documents', {
+  state: () => ({
+    documents: [] as Document[],
+    activeDocument: null as number | null,
+    searchQuery: '' as string,
+    error: '' as string,
+    isLoading: false as boolean,
+  }),
+  actions: {
+    setActiveDocument(id: number) {
+      this.activeDocument = id
+    },
+    setDocuments(docs: Document[]) {
+      this.documents = docs
+    },
+    setSearchQuery(query: string) {
+      this.searchQuery = query
+    },
+    async loadDocuments() {
+      try {
+        this.isLoading = true
+        const data = await fetchDocumentsApi(this.searchQuery)
+
+        this.setDocuments(data)
+
+        this.error = ''
+      } catch (err) {
+        this.error = 'Ошибка при загрузке документов'
+
+        this.setDocuments([])
+      } finally {
+        this.isLoading = false
+      }
+    },
+  },
+})
