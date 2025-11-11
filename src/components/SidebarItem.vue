f917c6bc9dfd9f7bdc731f946d8cda24acf5d5b4
Anastasiya Pozemina <a.pozyomina@voodoo.rocks>
2025-11-11 17:32:00 +0300

    Create Sidebar component

---
+++ src/components/SidebarItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <aside class="sidebar">
+    <div class="search-bar">
+      <div class="title text-title">Поиск документа</div>
+      <InputItem v-model="documentId" type="text" placeholder="Введите ID документа" />
+    </div>
+
+    <div>
+      <div class="title text-title">Результаты</div>
+      <div class="text-secondary">Ничего не найдено</div>
+      <div>результаты</div>
+    </div>
+  </aside>
+</template>
+
+<script setup lang="ts">
+import InputItem from '@/components/InputItem.vue'
+import { ref } from 'vue'
+
+const documentId = ref<string>('')
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+  padding: 30px;
+  border-right: 1px solid $color-separator;
+
+  .search-bar {
+    margin-bottom: 30px;
+  }
+
+  .title {
+    margin-bottom: 14px;
+  }
+}
+
+</style>
