2a2f96432c8fe92d58c139440c77971f7bfdc251
Anastasiya Pozemina <a.pozyomina@voodoo.rocks>
2025-11-11 17:31:37 +0300

    Create input component

---
+++ src/components/InputItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <input
+    class="input"
+    :type="type"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="handleInput"
+  />
+</template>
+
+<script setup lang="ts">
+import { debounce } from '@/utils/debounce'
+
+interface Props {
+  type?: 'text' | 'number' | 'email' | 'password' | 'tel' | 'url'
+  placeholder?: string
+  modelValue?: string | number
+}
+
+interface Emits {
+  (event: 'update:model-value', value: string | number): void
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  type: 'text',
+  placeholder: '',
+  modelValue: '',
+})
+
+const emit = defineEmits<Emits>()
+
+const debouncedEmit = debounce((value: string | number) => {
+  emit('update:model-value', value)
+}, 500)
+
+const handleInput = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  let value: string | number = target.value
+
+  if (props.type === 'number' && target.value) {
+    value = target.valueAsNumber || Number(target.value)
+  }
+
+  debouncedEmit(value)
+}
+</script>
+
+<style lang="scss" scoped>
+.input {
+  font-family: $font-family;
+  padding-left: 24px;
+  padding-right: 24px;
+  height: 49px;
+  box-sizing: border-box;
+  border-radius: $border-radius;
+  color: $color-dark;
+  border: 2px solid $gray-200;
+
+  &::placeholder {
+    color: $color-secondary;
+  }
+}
+</style>
