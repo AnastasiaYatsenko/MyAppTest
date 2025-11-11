43bd17d6e2545c39460f61f38f1500a894c38cf0
Anastasiya Pozemina <a.pozyomina@voodoo.rocks>
2025-11-11 11:08:01 +0300

    Initial commit

---
+++ src/component/HeaderItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <h1>MyApp</h1>
+    <div>Username</div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: $black;
+}
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-layout">
+    <div class="container">
+      <header-item />
+    </div>
+
+    <div class="container content">
+      <main>
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HeaderItem from '@/component/HeaderItem.vue'
+</script>
+
+<style lang="scss" scoped>
+.main-layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  padding: 40px;
+  box-sizing: border-box;
+
+  .content {
+    flex: 1;
+    background-color: $container;
+    border-radius: $content-border-radius;
+    box-shadow: $shadow-1;
+  }
+}
+</style>
